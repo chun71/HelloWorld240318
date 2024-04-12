4867f7a20ac1dfc3a46904ad647c9dc741a0ce86
chun71 <leechiachun71@gmail.com>
2024-04-12 13:17:52 +0800

    IDpapper

---
+++ Dapper/IDapper.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace HelloWorld240318.Dapper
+{
+    public interface IDapper
+    {
+        IEnumerable<T> Query<T>(string sql, object? dp = null, CommandType commandType = CommandType.Text, string connType = "QA");
+        IEnumerable<TReturn> Query<TFirst, TSecond, TReturn>(string sql, Func<TFirst, TSecond, TReturn> map, object? dp = null, string splitOn = "ColumnName", CommandType commandType = CommandType.Text, string connType = "QA");
+        int Execute(string sql, object? param, CommandType commandType = CommandType.Text, string connType = "QA");
+    }
+}
